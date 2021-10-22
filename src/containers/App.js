0e13d6865143589060b698677283290f5de28acc
Shangcasm <trishaloberianocapangpangan@gmail.com>
2021-10-22 16:30:03 +0800

    Redux continued

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,57 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import SearchBox from "../components/SearchBox";
-import Scroll from "../components/Scroll";
-import CardList from "../components/CardList";
-import ErrorBoundry from  "../components/ErrorBoundry";
-import "./App.css";
+import { setSearchField, requestRobots } from '../actions';
 
-import { setSearchfield } from '../action'
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 
-const mapStateToProps = state => {
-    return {
-        searchField: state.searchField
-    }
+import './App.css';
+
+// parameter state comes from index.js provider store state(rootReducers)
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending
+  }
 }
 
-const mapDispatchToTProps = (dispatch) => {
-    return{
-        onSearchChange: (event) => dispatch(setSearchfield(event.target.value))
-    }
+// dispatch the DOM changes to call an action. note mapStateToProps returns object, mapDispatchToProps returns function
+// the function returns an object then uses connect to change the data from redecers.
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
 }
 
 class App extends Component {
-    constructor() {
-      super()
-      this.state = {
-        robots: []
-      }
-    }
-  
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response=> response.json())
-            .then(users => {this.setState({ robots: users})});
-    }
-  
-    render() {
-      const { robots, searchfield } = this.state;
-      const { searchField, onSearchChange } = this.props
-      const filteredRobots = robots.filter(robot =>{
-        return robot.name.toLowerCase().includes(searchField.toLowerCase());
-      })
-      return !robots.length ?
-        <h1>Loading</h1> :
-        (
-          <div className='tc'>
-            <h1 className='f1'>RoboFriends</h1>
-            <SearchBox searchChange={onSearchChange}/>
-            <Scroll>
-              <CardList robots={filteredRobots} />
-            </Scroll>
-          </div>
-        );
-    }
+  componentDidMount() {
+    this.props.onRequestRobots();
   }
-  
-  export default connect(mapStateToProps, mapDispatchToTProps)(App);
+
+  render() {
+    const { robots, searchField, onSearchChange, isPending } = this.props;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    return (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          { isPending ? <h1>Loading</h1> :
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          }
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+// action done from mapDispatchToProps will channge state from mapStateToProps
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect } from 'react-redux';
-import './index.css'
-import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import 'tachyons';
-import { createStore } from 'redux';
-import { searchRobots } from './reducers';
 
-const store = createStore(searchRobots)
+import App from './containers/App';
+// import registerServiceWorker from './registerServiceWorker';
+import { requestRobots, searchRobots } from './reducers'
+
+import './index.css';
+
+const logger = createLogger() 
+
+const rootReducers = combineReducers({requestRobots, searchRobots})
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-                <Provider store={store}>
-                    <App />
-                </Provider>,  document.getElementById('root')
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// registerServiceWorker();
